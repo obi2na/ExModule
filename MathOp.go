98a4eeb4d2e217e1d5df0df2a98ae5443bc09923
aniekanessien <obinna.a.essien@gmail.com>
2025-05-06 19:17:03 -0400

    module changes

--- MathOp.go
+++ MathOp.go
@@ -1,5 +1,7 @@
 package ExModule
 
+// This add function just does what it says
+// it adds to values and returns the result of the addition
 func Add(a, b int) int {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/obi2na/ExModule
 
-go 1.24
+go 1.24.3
